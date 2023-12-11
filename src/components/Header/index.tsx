0f2cd1ec7575a278d7ccc0d70d64d8808474bbc5
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2023-12-11 12:21:13 -0300

    Adjusting Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,46 @@
+'use client'
+
 import styles from './styles.module.css'
 
+import { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import OpenMenuIcon from '@/assets/svg/open_menu.svg'
+
 const Header = () => {
+    const [openMenu, setOpenMenu] = useState<boolean>()
+
+    const handleOpenMenu = (): void => {
+        setOpenMenu(!openMenu)
+    }
+
     return (
-        <div>Header</div>
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <span>Victor Santos</span>
+            </div>
+            <nav className={`${styles.nav} ${openMenu ? '' : styles.hiden}`}>
+                {/* <nav className={`${styles.nav}`}> */}
+                <ul className={styles.menu}>
+                    <li>
+                        <Link href='/'>Áreas</Link>
+                    </li>
+                    <li>
+                        <Link href='/'>Sobre</Link>
+                    </li>
+                    <li>
+                        <Link href='/'>Casos</Link>
+                    </li>
+                    <li>
+                        <Link href='/'>Contato</Link>
+                    </li>
+                </ul>
+            </nav>
+            <button className={styles.handler_menu} onClick={handleOpenMenu}>
+                <Image src={OpenMenuIcon} alt='Icons8 Menu Hamburguer' />
+            </button>
+        </header>
     )
 }
 
