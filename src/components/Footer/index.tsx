ea85cd99fe49502a0249f7a1976aaa3d8ca8e761
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2023-12-10 23:13:15 -0300

    Adjusting MoreInformation and Footer components

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,24 +3,11 @@ import styles from './styles.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import InstagramIcon from '@/assets/svg/instagram.svg'
-
 const Footer = () => {
     return (
         <section className={styles.footer}>
             <div className={styles.logo}>
                 <span>Victor Santos</span>
-            </div>
-            <div className={styles.social_medias}>
-                <Link href='/' target='_blank'>
-                    <Image src={InstagramIcon} alt='Icons8 Instagram' />
-                </Link>
-                <Link href='/' target='_blank'>
-                    <Image src={InstagramIcon} alt='Icons8 Instagram' />
-                </Link>
-                <Link href='/' target='_blank'>
-                    <Image src={InstagramIcon} alt='Icons8 Instagram' />
-                </Link>
             </div>
             <div className={styles.reserved}>
                 <p>Copyright © Victor Santos - 2023</p>
--- src/components/MoreInformation/index.tsx
+++ src/components/MoreInformation/index.tsx
@@ -1,5 +1,10 @@
 import styles from './styles.module.css'
 
+import Link from 'next/link'
+import Image from 'next/image'
+
+import InstagramIcon from '@/assets/svg/instagram.svg'
+
 const MoreInformation = () => {
     return (
         <section className={styles.more_information}>
@@ -18,6 +23,20 @@ const MoreInformation = () => {
             <div className={styles.info}>
                 <span>Endereço</span>
                 <p>Rua Jamel Cecílio N45</p>
+            </div>
+            <div className={styles.info}>
+                <span>Mídias sociais</span>
+                <div className={styles.social_medias}>
+                    <Link href='/' target='_blank'>
+                        <Image src={InstagramIcon} alt='Icons8 Instagram' />
+                    </Link>
+                    <Link href='/' target='_blank'>
+                        <Image src={InstagramIcon} alt='Icons8 Instagram' />
+                    </Link>
+                    <Link href='/' target='_blank'>
+                        <Image src={InstagramIcon} alt='Icons8 Instagram' />
+                    </Link>
+                </div>
             </div>
         </section>
     )
